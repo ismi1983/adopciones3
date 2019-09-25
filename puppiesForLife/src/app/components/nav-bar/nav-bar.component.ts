e04a73eb51c91b9f5d838c987e3d2059ff9b97ab
Urbano <urbano.gonzalez@alumnos.udg.mx>
2019-09-25 17:42:29 -0500

    Working on navbar component

---
+++ puppiesForLife/src/app/components/nav-bar/nav-bar.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { faBars} from '@fortawesome/free-solid-svg-icons';
+import { faFacebookF, faTwitter, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent implements OnInit {
+  faBars = faBars;
+  faFacebook = faFacebookF;
+  faYoutube = faYoutube;
+  faTwitter = faTwitter;
+  faInstagram = faInstagram
+
+  constructor() {
+   }
+
+  ngOnInit() {
+  }
+
+}
