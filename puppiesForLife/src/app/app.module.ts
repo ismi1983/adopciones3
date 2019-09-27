913b076bfb0a9b6c0f404eb4c7109ea589a5b2e3
Urbano <urbano.gonzalez@alumnos.udg.mx>
2019-09-27 15:28:21 -0500

    Login component created

--- puppiesForLife/src/app/app.module.ts
+++ puppiesForLife/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HomeComponent } from './components/home/home.component';
 import { TableroComponent } from './components/tablero/tablero.component';
 import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './components/login/login.component';
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     HomeComponent,
-    TableroComponent
+    TableroComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
