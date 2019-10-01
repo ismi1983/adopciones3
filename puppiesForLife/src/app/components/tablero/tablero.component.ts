8026e6320d8bd2e7a648a619c0d7c0635ba19c45
Urbano <urbano.gonzalez@alumnos.udg.mx>
2019-09-30 19:12:21 -0500

    Icon added to deelte button

--- puppiesForLife/src/app/components/tablero/tablero.component.ts
+++ puppiesForLife/src/app/components/tablero/tablero.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DogsService } from '../../services/dogs.service'
 import { Dog} from '../../models/dogs'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-tablero',
@@ -8,7 +9,7 @@ import { Dog} from '../../models/dogs'
   styleUrls: ['./tablero.component.css']
 })
 export class TableroComponent implements OnInit {
-
+  faTrash = faTrash;
   constructor( private dogService:DogsService ) { }
 
 dogs$:Dog[];
@@ -21,7 +22,9 @@ dogs$:Dog[];
    this.getDogs();
   }
   deleteDog(dog:Dog):void{ 
-    this.dogs$ = this.dogs$.filter(d => d !== dog);   
+    // Takes out the dog form the view
+    this.dogs$ = this.dogs$.filter(d => d !== dog);  
+    // pass the dog object to the service 
     this.dogService.deleteDog(dog).subscribe();
   }
 }
